fe3e455fc1bd8fe78ec715b07283eeba97d92cfa
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-06-10 22:50:43 -0600

    update cfg

---
+++ terraform/cfg.md
@@ -0,0 +1,3 @@
+terraform init
+terraform plan (pre-execution previewer)
+terraform apply (to execute)
--- terraform/test.tf
+++ terraform/test.tf
@@ -1,5 +1,21 @@
+//login credentials
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
     region     = "us-east-2"
+}
+
+//Create resource:
+resource "aws_instance" "test_Terraform_Resource" {
+    count = 2
+    ami = "ami-0d8d212151031f51c"
+    instance_type = "t2.micro"
+
+}
+
+resource "aws_instance" "test_Terraform_Resource2" {
+
+    ami = "ami-0d8d212151031f51c"
+    instance_type = "t2.micro"
+
 }
