d7debd8bbe9c4e0b4a1a8c2bac00257b50478aab
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-06-10 23:15:30 -0600

    update test

--- terraform/cfg.md
+++ terraform/cfg.md
@@ -1,3 +1,4 @@
 terraform init
 terraform plan (pre-execution previewer)
 terraform apply (to execute)
+terraform destroy (to delete the infrastructure)
--- terraform/test.tf
+++ terraform/test.tf
@@ -11,11 +11,20 @@ resource "aws_instance" "test_Terraform_Resource" {
     ami = "ami-0d8d212151031f51c"
     instance_type = "t2.micro"
 
+    tags = {
+        Name = "Amazon Linux 2"
+        Owner = "Vadym Hulchenko"
+        Project = "Terraform Learning"
+    }
+
 }
 
 resource "aws_instance" "test_Terraform_Resource2" {
-
     ami = "ami-0d8d212151031f51c"
-    instance_type = "t2.micro"
-
+    instance_type = "t3.micro"
+    tags = {
+        Name = "Amazon Linux 2"
+        Owner = "Vadym Hulchenko"
+        Project = "Terraform Learning"
+    }
 }
