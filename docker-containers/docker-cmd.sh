a2241f6b610057812674972c6ce8c54013ae97a7
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-06-13 00:52:57 -0600

    update clearcache

--- docker-containers/docker-cmd.sh
+++ docker-containers/docker-cmd.sh
@@ -1,10 +1,11 @@
-# docker pull
-# docker start
+# to avoid using sudo commands every time, use: sudo usermod -aG docker $USER
+# docker pull <image_name> -> downloads an image
+# docker start <image_name>
 # docker run (docker pull + docker start)
 # docker run <IMAGE NAME> (pulls the latest version of an image & starts the container) 
 #	-d (detached mode / background run)
 #	--rm (auto-removes container on stop)
-#	-p 6000:6379 (bind host port to container port)(only one can be bind to the same local port, ex: -p 27017:27017(MongoDB default port))
+#	-p 6000:6379 (bind host port to container port, so the entry now is 6000)(only one can be bind to the same local port, ex: -p 27017:27017(MongoDB default port))
 #	--name <CUSTOM NAME> (to rename a container)
 #   -e (environment variables)
 #   -v (volume, to indicate directory to the remote volume, ex: -v /home/mount/data:/var/lib/mysql/data [local directory]:[remote volume directory])
